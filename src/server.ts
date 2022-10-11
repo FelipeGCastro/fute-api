966c3a834bed1bd36ddd63226f62fc2330b1d194
Luiz Castro <luiz.castro@gaspardbruno.com>
2022-10-12 00:49:33 +0100

    add console log to endpoint

--- src/server.ts
+++ src/server.ts
@@ -49,6 +49,7 @@ io.on('connection', socket => {
 })
 
 app.get('/healthcheck', (req, res) => {
+  console.log('Health Check called')
   res.send('Express + TypeScript Server is running now')
 })
 httpServer.listen(port, () => {
